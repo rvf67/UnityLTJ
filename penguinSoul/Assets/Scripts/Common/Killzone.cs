62b3f000318ad57a44cae150b60af8d0129036ed
rvf67 <62585538+rvf67@users.noreply.github.com>
2024-08-25 21:09:17 +0900

    보스체력 UI구현 중

--- penguinSoul/Assets/Scripts/Common/Killzone.cs
+++ penguinSoul/Assets/Scripts/Common/Killzone.cs
@@ -5,19 +5,6 @@ using UnityEngine;
 
 public class Killzone : MonoBehaviour
 {
-    GameManager gameManager;
-
-    Camera cam;
-    private void Awake()
-    {
-        gameManager = GetComponent<GameManager>();
-        cam=Camera.main;
-    }
-    private void Update()
-    {
-        float dirx = cam.transform.position.x;
-        transform.position=new Vector2(dirx, 0);
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // GetComponent를 했을 때 <>사이의 클래스나 그 클래스를 상속받은 클래스가 없으면 return은 null
