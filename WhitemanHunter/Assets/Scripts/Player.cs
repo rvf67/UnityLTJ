1539d6560c07382fe03817f5b6bf0fa776ab7434
rvf67 <62585538+rvf67@users.noreply.github.com>
2024-10-01 13:51:22 +0900

    플레이어 이동구현

--- WhitemanHunter/Assets/Scripts/Player.cs
+++ WhitemanHunter/Assets/Scripts/Player.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 direction { get; private set; }
+    public void OnMoveInput(InputAction.CallbackContext context)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector2 input =context.ReadValue<Vector2>();
+        direction = new Vector3(input.x,0f,input.y);
     }
 }
